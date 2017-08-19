0e0bb593b7b6054b4e79ff14ab30d756427cfb87
tomascus <arbiter1000@gmail.com>
2017-08-19 23:22:37 +1000

    Merge tag 'v3.4.113' into HEAD
    This is the 3.4.113 stable release
    
    Change-Id: I2e4e93bc1dbeb27d98fa609003e7182afd95cff5

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -445,6 +445,8 @@ asmlinkage void __exception do_undefinstr(struct pt_regs *regs)
 
 	trace_undef_instr(regs, (void *)pc);
 
+die_sig:
+
 #ifdef CONFIG_DEBUG_USER
 	if (user_debug & UDBG_UNDEFINED) {
 		printk(KERN_INFO "%s (%d): undefined instruction: pc=%p\n",
--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1398,6 +1398,7 @@ int __dwc3_gadget_ep_set_halt(struct dwc3_ep *dep, int value)
 		} else {
 			dep->flags |= DWC3_EP_STALL;
 		}
+
 	} else {
 		ret = dwc3_send_gadget_ep_cmd(dwc, dep->number,
 			DWC3_DEPCMD_CLEARSTALL, &params);
